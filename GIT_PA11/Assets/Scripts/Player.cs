4b1511e0f4d69ce1be9db57524c1492d3f4b5e38
XuanWei-CPX <64126391+XuanWei-CPX@users.noreply.github.com>
2020-06-19 15:27:32 +0800

    game
    updated by xuan wei

---
+++ GIT_PA11/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        scoreText.text = "Score : " + score;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Score"))
+        {
+            score += 1;
+        }
+    }
+}
---
+++ GIT_PA11/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] SpawnObject;
+    float PositionY;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    private int randEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+
+    }
+
+    //void SpawnObjects()
+    //{
+    //    PositionY = Random.Range(4, -4f);
+    //    this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+    //    Instantiate(SpawnObject, transform.position, transform.rotation);
+    //}
+
+    IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while(!stop)
+        {
+            randEnemy = Random.Range(0, 3);
+            PositionY = Random.Range(4, -4f);
+            this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+            Instantiate(SpawnObject[randEnemy], transform.position, transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
